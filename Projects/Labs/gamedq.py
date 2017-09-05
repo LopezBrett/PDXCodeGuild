568ca9280ac8943900c310b1ca5c5b181c33852e
LopezBrett <lopez.t.brett@gmail.com>
2017-09-05 11:20:47 -0700

    Add lab file

---
+++ Projects/Labs/gamedq.py
@@ -0,0 +1,8 @@
+# character
+# players pick character
+# players enter name
+# select dungeon size
+# start of player turn random tile flip
+# player choose flip
+# player move
+# win conditions / first to reach the center
